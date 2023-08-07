46a8d0dd19e34fe3cb3fdc8a1096221f467645af
liuyuzhen <liuyuzhen@cqhorizon.com>
2023-08-07 15:42:51 +0800

    将嵌套循环转变为哈希表查找，时间复杂度可从O(n^2)变为O(n)

--- 1/main.go
+++ 1/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 /*
 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
@@ -18,17 +16,57 @@ import "fmt"
 输出：[0,1]
 */
 
-var nums = []int{3, 3}
-var target = 6
-
-func main() {
+func twoSum1(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] != target {
 				continue
 			} else {
-				fmt.Println(i, j)
+				return []int{i, j}
 			}
 		}
 	}
+	return nil
+}
+
+func twoSum2(nums []int, target int) []int {
+	hashMap := make(map[int]int)
+	for i, v := range nums {
+		anotherNum := target - v
+		if index, ok := hashMap[anotherNum]; ok {
+			return []int{index, i}
+		} else {
+			hashMap[v] = i
+		}
+	}
+	return nil
+}
+
+func twoSum(nums []int, target int) []int {
+	// 创建一个哈希表，用来存放数组的元素值以及索引值
+	// key 是数组中的元素值，value 是数组中元素值的索引
+	hashMap := make(map[int]int)
+	// 遍历整个数组
+	for i, num := range nums {
+		// 计算与当前元素配对的目标值
+		anotherNum := target - num
+		// 判断哈希表中是否存在配对的元素
+		index, exists := hashMap[anotherNum]
+		if exists {
+			// 如果存在，返回两个数的索引
+			return []int{index, i}
+		} else {
+			// 如果不存在，将当前元素及其索引添加到哈希表中
+			hashMap[num] = i
+		}
+	}
+	// 如果找不到配对的元素，返回空数组
+	return nil
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	//twoSum1(nums, target)
+	twoSum(nums, target)
 }
