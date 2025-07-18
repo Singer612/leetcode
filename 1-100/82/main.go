e07dad7c62d6769851446e029af9f5e05dbaf715
Singer612 <15530389867@163.com>
2025-07-18 14:48:14 +0800

    82 做烦了，还没写出来

---
+++ 1-100/82/main.go
@@ -0,0 +1,33 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	dummy := &ListNode{
+		Val:  -1,
+		Next: head,
+	}
+	cur := dummy
+
+	for cur.Next != nil && cur.Next.Next != nil {
+		if cur.Next.Val == cur.Next.Next.Val {
+			x := cur.Next.Val
+			for cur.Next != nil && cur.Next.Val == x {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummy.Next
+}
+
+func main() {
+
+}
