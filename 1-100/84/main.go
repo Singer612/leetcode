2147bbdf67f05d55695f9d6ed0f8552cf23de163
Singer612 <15530389867@163.com>
2025-07-23 14:34:19 +0800

    84 再刷

---
+++ 1-100/84/main.go
@@ -0,0 +1,50 @@
+package main
+
+func largestRectangleArea(heights []int) int {
+	n := len(heights)
+	// 向左向右分别找到最小的数
+	left, right := make([]int, n), make([]int, n)
+	// 单调栈
+	stack := []int{}
+	// 从左向右
+	for i := 0; i < n; i++ {
+		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[i] {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) == 0 {
+			left[i] = -1
+		} else {
+			left[i] = stack[len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	// 从右向左
+	stack = []int{}
+	for i := n - 1; i >= 0; i-- {
+		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[i] {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) == 0 {
+			right[i] = n
+		} else {
+			right[i] = stack[len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans = max(ans, (right[i]-left[i]-1)*heights[i])
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+
+}
