7430444e12be0981db54362868111db78aa6706b
Singer612 <15530389867@163.com>
2025-07-27 17:16:43 +0800

    454 四数相加II

---
+++ 401-500/454/main.go
@@ -0,0 +1,23 @@
+package main
+
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+	m := make(map[int]int)
+	count := 0
+	for _, v1 := range nums1 {
+		for _, v2 := range nums2 {
+			m[v1+v2]++
+		}
+	}
+	for _, v3 := range nums3 {
+		for _, v4 := range nums4 {
+			if v, ok := m[0-v3-v4]; ok {
+				count = count + v
+			}
+		}
+	}
+	return count
+}
+
+func main() {
+
+}
