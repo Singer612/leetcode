7b59b6ae9bd07a435ee9708ae6ad922954d4c3a4
Singer612 <15530389867@163.com>
2025-07-27 21:01:07 +0800

    344 反转字符串

---
+++ 301-400/344/main.go
@@ -0,0 +1,15 @@
+package main
+
+func reverseString(s []byte) {
+	left, right := 0, len(s)-1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
+
+func main() {
+	s := []byte{'H', 'a', 'n', 'n', 'a', 'h'}
+	reverseString(s)
+}
