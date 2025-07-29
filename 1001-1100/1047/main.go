e11cb56520d23810353b7b6a7ae2c57bd49b47da
Singer612 <15530389867@163.com>
2025-07-29 17:40:17 +0800

    1047 删除字符串中的所有相邻重复项

--- 1001-1100/1047/main.go
+++ 1001-1100/1047/main.go
@@ -5,7 +5,6 @@ func removeDuplicates(s string) string {
 	for _, v := range s {
 		if len(stack) != 0 && v == stack[len(stack)-1] {
 			stack = stack[:len(stack)-1]
-			continue
 		} else {
 			stack = append(stack, v)
 		}
