e560da75d3b3b05cbabf3c7b708695137c884617
Singer612 <15530389867@163.com>
2025-07-30 17:09:43 +0800

    145 二叉树的后序遍历 递归

---
+++ 101-200/145/main.go
@@ -0,0 +1,26 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	res := make([]int, 0)
+	var postorder func(node *TreeNode)
+	postorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		postorder(node.Left)
+		postorder(node.Right)
+		res = append(res, node.Val)
+	}
+	postorder(root)
+	return res
+}
+
+func main() {
+
+}
