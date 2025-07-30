5f18a17fea5c135cd8a08ebab8cfa7b213f348a1
Singer612 <15530389867@163.com>
2025-07-30 19:39:19 +0800

    144 二叉树的前序遍历 迭代

--- 101-200/144/main.go
+++ 101-200/144/main.go
@@ -16,7 +16,7 @@ type TreeNode struct {
 //	return
 //}
 
-func preorderTraversal(root *TreeNode) []int {
+func preorderTraversal1(root *TreeNode) []int {
 	res := make([]int, 0)
 	var preorder func(node *TreeNode)
 	preorder = func(node *TreeNode) {
@@ -29,6 +29,57 @@ func preorderTraversal(root *TreeNode) []int {
 		return
 	}
 	preorder(root)
+	return res
+}
+
+type Stack struct {
+	stack []*TreeNode
+}
+
+func New() *Stack {
+	return &Stack{
+		stack: make([]*TreeNode, 0),
+	}
+}
+func (s *Stack) Pop() {
+	if !s.IsEmpty() {
+		s.stack = s.stack[:len(s.stack)-1]
+	}
+	return
+}
+func (s *Stack) Push(node *TreeNode) {
+	s.stack = append(s.stack, node)
+
+}
+func (s *Stack) Top() *TreeNode {
+	if !s.IsEmpty() {
+		return s.stack[len(s.stack)-1]
+	}
+	return nil
+}
+func (s *Stack) IsEmpty() bool {
+	return len(s.stack) == 0
+}
+
+// 迭代，使用栈
+func preorderTraversal(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+	s := New()
+	s.Push(root)
+	for !s.IsEmpty() {
+		node := s.Top()
+		res = append(res, node.Val)
+		s.Pop()
+		if node.Right != nil {
+			s.Push(node.Right)
+		}
+		if node.Left != nil {
+			s.Push(node.Left)
+		}
+	}
 	return res
 }
 
