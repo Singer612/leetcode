fdcb64aea61d04e4ba164de2259c449b162fa8c3
Singer612 <15530389867@163.com>
2025-07-31 11:12:44 +0800

    111 二叉树的最小深度

---
+++ 101-200/111/main.go
@@ -0,0 +1,67 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+type Queue struct {
+	queue []*TreeNode
+	size  int
+}
+
+func New() *Queue {
+	return &Queue{
+		queue: make([]*TreeNode, 0),
+		size:  0,
+	}
+}
+func (q *Queue) Push(node *TreeNode) {
+	q.queue = append(q.queue, node)
+	q.size++
+}
+func (q *Queue) Pop() {
+	q.queue = q.queue[1:]
+	q.size--
+}
+func (q *Queue) Top() *TreeNode {
+	return q.queue[0]
+}
+func (q *Queue) IsEmpty() bool {
+	return q.size == 0
+}
+
+func minDepth(root *TreeNode) int {
+	res := make([][]int, 0)
+	depth := 0
+	if root == nil {
+		return 0
+	}
+	q := New()
+	q.Push(root)
+	for !q.IsEmpty() {
+		depth++
+		size := q.size
+		tmp := make([]int, 0)
+		for i := 0; i < size; i++ {
+			node := q.Top()
+			tmp = append(tmp, node.Val)
+			q.Pop()
+			if node.Left == nil && node.Right == nil {
+				return depth
+			}
+			if node.Left != nil {
+				q.Push(node.Left)
+			}
+			if node.Right != nil {
+				q.Push(node.Right)
+			}
+		}
+		res = append(res, tmp)
+	}
+	return depth
+}
+
+func main() {
+
+}
