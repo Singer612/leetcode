893062911bc743bab404f049b35a6496e7a1060c
Singer612 <15530389867@163.com>
2025-07-31 16:01:27 +0800

    222 完全二叉树的节点个数 后序 直觉写法

--- 201-300/222/main.go
+++ 201-300/222/main.go
@@ -10,20 +10,7 @@ func countNodes(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	sum := 1
-	var count func(node *TreeNode)
-	count = func(node *TreeNode) {
-		if node.Left != nil {
-			count(node.Left)
-			sum++
-		}
-		if node.Right != nil {
-			count(node.Right)
-			sum++
-		}
-	}
-	count(root)
-	return sum
+	return countNodes(root.Left) + countNodes(root.Right) + 1
 }
 
 func main() {
