a455f6b640cf19685c19922f3309eee877025a14
Singer612 <15530389867@163.com>
2025-08-03 22:20:35 +0800

    701 二叉搜索树中的插入操作

---
+++ 701-800/701/main.go
@@ -0,0 +1,44 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	tmp := &TreeNode{
+		Val: val,
+	}
+	if root == nil {
+		return tmp
+	}
+	var insert func(node *TreeNode, val int, flg bool)
+	insert = func(node *TreeNode, val int, flg bool) {
+		if flg {
+			return
+		}
+		if val > node.Val {
+			if node.Right == nil {
+				node.Right = tmp
+				insert(node.Right, val, true)
+			} else {
+				insert(node.Right, val, false)
+			}
+
+		}
+		if val < node.Val {
+			if node.Left == nil {
+				node.Left = tmp
+				insert(node.Left, val, true)
+			} else {
+				insert(node.Left, val, false)
+			}
+		}
+	}
+	insert(root, val, false)
+	return root
+}
+func main() {
+
+}
