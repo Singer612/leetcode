0e233e721330b701576b0109586c550c86eeca9c
Singer612 <15530389867@163.com>
2025-08-03 17:02:07 +0800

    617 合并二叉树

---
+++ 601-700/617/main.go
@@ -0,0 +1,48 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil && root2 != nil {
+		return root2
+	}
+	if root1 != nil && root2 == nil {
+		return root1
+	}
+	if root1 == nil && root2 == nil {
+		return nil
+	}
+	var merge func(root1 *TreeNode, root2 *TreeNode)
+	merge = func(root1 *TreeNode, root2 *TreeNode) {
+		if root2 == nil || root1 == nil {
+			return
+		}
+		if root1 != nil && root2 != nil {
+			root1.Val = root1.Val + root2.Val
+		}
+		// 做平移逻辑
+		if root1.Left == nil && root2.Left != nil {
+			root1.Left = root2.Left
+			merge(root1.Left, nil)
+		} else {
+			merge(root1.Left, root2.Left)
+		}
+		if root1.Right == nil && root2.Right != nil {
+			root1.Right = root2.Right
+			merge(root1.Right, nil)
+		} else {
+			merge(root1.Right, root2.Right)
+		}
+		return
+	}
+	merge(root1, root2)
+	return root1
+}
+
+func main() {
+
+}
