c5ec00ce4432af46bb95bb55511423e62cede0bf
Singer612 <15530389867@163.com>
2025-08-08 14:31:15 +0800

    77 组合

---
+++ 1-100/77/main.go
@@ -0,0 +1,25 @@
+package main
+
+func combine(n int, k int) [][]int {
+	path, res := make([]int, 0), make([][]int, 0)
+	var dfs func(n, k, start int)
+	dfs = func(n, k, start int) {
+		if len(path) == k {
+			tmp := make([]int, 0)
+			tmp = append(tmp, path...)
+			res = append(res, tmp)
+			return
+		}
+		for i := start; i <= n; i++ {
+			path = append(path, i)
+			dfs(n, k, i+1)
+			path = path[:len(path)-1]
+		}
+	}
+	dfs(n, k, 1)
+	return res
+}
+
+func main() {
+
+}
