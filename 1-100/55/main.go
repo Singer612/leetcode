f2092a59c892ae1135892a7820d11561d8753e7f
Singer612 <15530389867@163.com>
2025-08-11 09:24:13 +0800

    55 跳跃游戏

---
+++ 1-100/55/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+}
+
+func canJump(nums []int) bool {
+	cover := 0
+	n := len(nums) - 1
+	for i := 0; i <= cover; i++ { // 每次与覆盖值比较
+		cover = max(i+nums[i], cover) //每走一步都将 cover 更新为最大值
+		if cover >= n {
+			return true
+		}
+	}
+	return false
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
