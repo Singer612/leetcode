94b588a01b727595f430b5a7c8630aa742daa865
Singer612 <15530389867@163.com>
2025-08-12 11:24:52 +0800

    452 用最少数量的箭引爆气球

---
+++ 401-500/452/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "sort"
+
+func main() {
+
+}
+
+func findMinArrowShots(points [][]int) int {
+	res := 1
+	sort.Slice(points, func(i, j int) bool {
+		return points[i][0] < points[j][0]
+	})
+	for i := 1; i < len(points); i++ {
+		if points[i-1][1] < points[i][0] {
+			res++
+		} else {
+			points[i][1] = min(points[i-1][1], points[i][1])
+		}
+	}
+	return res
+}
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
